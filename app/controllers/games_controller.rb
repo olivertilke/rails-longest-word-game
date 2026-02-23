062e9ff984dbc8aa83bb651785e698102e7760e6
Oliver Tilke <olivertilke@me.com>
2026-02-23 15:27:40 +0100

    solution no styling not cool looking yet

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,5 +9,25 @@ class GamesController < ApplicationController
   end
 
   def score
+    @word = params[:word].upcase
+    # string to array now
+    @letters = params[:letters].split
+
+    # first check: did user only take letters from letter grid?
+    word_in_grid = @word.chars.all? { |letter| @word.count(letter) <= @letters.count(letter) }
+
+    # 2nd check for real englisch word
+    response = URI.open("https://dictionary.lewagon.com/#{@word}")
+    json = JSON.parse(response.read)
+    english_word = json["found"]
+
+    # urteil/judgement
+    if !word_in_grid
+      @result = "Sorry but <b>#{@word}</b> can't be built out of #{@letters.join(', ')}, bro! 🙈"
+    elsif !english_word
+      @result = "Sorry but <b>#{@word}</b> does not seem to be a valid English word, bro... You should learn english first! 🤪🚀"
+    else
+      @result = "Congratulations! <b>#{@word}</b> is a valid English word, bro 🍾🎉!"
+    end
   end
 end
